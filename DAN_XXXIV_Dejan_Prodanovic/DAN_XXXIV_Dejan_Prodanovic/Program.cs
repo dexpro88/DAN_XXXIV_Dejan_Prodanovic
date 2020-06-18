3e5a400315251b2612a0c77d72fb2500da9455b8
dexpro88 <dejan.prodanovic@panonit.com>
2020-06-18 00:50:43 -0700

    created class Bank with method Withdraw

--- DAN_XXXIV_Dejan_Prodanovic/DAN_XXXIV_Dejan_Prodanovic/Program.cs
+++ DAN_XXXIV_Dejan_Prodanovic/DAN_XXXIV_Dejan_Prodanovic/Program.cs
@@ -2,14 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DAN_XXXIV_Dejan_Prodanovic
 {
-    class Program
+    class Bank
     {
-        static void Main(string[] args)
+        private object thisLock = new object();
+        int totalAmount = 10000;
+        Random rnd = new Random();
+        
+        int Withdraw(int ammountToWithdraw)
         {
+            if (totalAmount < 0)
+            {
+                throw new Exception("Usli ste u crveno");
+            }
+            lock (thisLock)
+            {
+                if (totalAmount >= ammountToWithdraw)
+                {
+
+                    Console.WriteLine("Stanje na racunu u banci: " + totalAmount);
+                    Console.WriteLine("Iznos koji podizete: " + ammountToWithdraw);
+                    totalAmount -= ammountToWithdraw;
+                    Console.WriteLine("Stanje na racunu nakon podizanja: " + totalAmount);
+                    return ammountToWithdraw;
+                }
+                else
+                {
+                    return 0; // Transakcija odbijena
+                }
+            }
+
         }
+       
     }
-}
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.ReadLine();
+            }
+        }
+
+    }
+ 
