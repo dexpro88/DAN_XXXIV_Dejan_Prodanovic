fca6214f86627379002281087a595e5a30dc1e61
dexpro88 <dejan.prodanovic@panonit.com>
2020-06-18 02:13:50 -0700

    Added input for number of clients

---
+++ DAN_XXXIV_Dejan_Prodanovic/DAN_XXXIV_Dejan_Prodanovic/Bank.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DAN_XXXIV_Dejan_Prodanovic
+{
+    class Bank
+    {
+        private object thisLock = new object();
+        public int TotalAmount { get; set; }
+        
+        private bool cashMashine1;
+        private bool cashMashine2;
+
+        public Bank()
+        {
+            TotalAmount = 10000;
+        }
+        int Withdraw(int ammountToWithdraw)
+        {
+            if (TotalAmount < 0)
+            {
+                throw new Exception("Crvena zona");
+            }
+
+            if (TotalAmount >= ammountToWithdraw)
+            {
+
+                Console.WriteLine("Stanje na racunu u banci: {0}", TotalAmount);
+                Console.WriteLine("Iznos koji klijent podize: {0}", ammountToWithdraw);
+                TotalAmount -= ammountToWithdraw;
+                Console.WriteLine("Stanje na racunu nakon podizanja: {0}\n", TotalAmount);
+                return ammountToWithdraw;
+            }
+            else
+            {
+                return 0; // Transakcija odbijena
+            }
+
+
+        }
+        public void DoTransactionsOnCashMashine1(int ammountToWithdraw)
+        {
+            lock (thisLock)
+            {
+                if (cashMashine2)
+                {
+
+                    while (cashMashine2)
+                    {
+                        Thread.Sleep(25);
+                    }
+
+                    Console.WriteLine("Klijent {0} pokusava da podigne {1} RSD sa bankomata1",
+                        Thread.CurrentThread.Name, ammountToWithdraw);
+                    cashMashine1 = true;
+                    int withdrawnAmount = Withdraw(ammountToWithdraw);
+                    if (withdrawnAmount == 0)
+                    {
+                        Console.WriteLine("Nema dovoljno novca na racunu banke");
+                    }
+                    Thread.Sleep(1000);
+                    cashMashine1 = false;
+
+                }
+                else
+                {
+                    Console.WriteLine("Klijent {0} pokusava da podigne {1} RSD sa bankomata1",
+                     Thread.CurrentThread.Name, ammountToWithdraw);
+                    cashMashine1 = true;
+                    int withdrawnAmount = Withdraw(ammountToWithdraw);
+                    if (withdrawnAmount == 0)
+                    {
+                        Console.WriteLine("Nema dovoljno novca na racunu banke");
+                    }
+                    Thread.Sleep(1000);
+                    cashMashine1 = false;
+                }
+            }
+        }
+
+        public void DoTransactionsOnCashMashine2(int ammountToWithdraw)
+        {
+            lock (thisLock)
+            {
+                if (cashMashine1)
+                {
+
+                    while (cashMashine1)
+                    {
+                        Thread.Sleep(25);
+                    }
+                    Console.WriteLine("Klijent {0} pokusava da podigne {1} RSD sa bankomata2",
+                     Thread.CurrentThread.Name, ammountToWithdraw);
+                    cashMashine2 = true;
+                    int witdrawnAmount = Withdraw(ammountToWithdraw);
+                    if (witdrawnAmount == 0)
+                    {
+                        Console.WriteLine("Nema dovoljno novca na racunu banke");
+                    }
+                    Thread.Sleep(1000);
+                    cashMashine2 = false;
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Klijent {0} pokusava da podigne {1} RSD sa bankomata2" +
+                        "",
+                     Thread.CurrentThread.Name, ammountToWithdraw);
+                    cashMashine2 = true;
+                    int witdrawnAmount = Withdraw(ammountToWithdraw);
+                    if (witdrawnAmount == 0)
+                    {
+                        Console.WriteLine("Nema dovoljno novca na racunu banke");
+                    }
+                    cashMashine2 = false;
+                }
+            }
+        }
+    }
+}
--- DAN_XXXIV_Dejan_Prodanovic/DAN_XXXIV_Dejan_Prodanovic/Program.cs
+++ DAN_XXXIV_Dejan_Prodanovic/DAN_XXXIV_Dejan_Prodanovic/Program.cs
@@ -7,117 +7,7 @@ using System.Threading.Tasks;
 
 namespace DAN_XXXIV_Dejan_Prodanovic
 {
-    class Bank
-    {
-        private object thisLock = new object();
-        int totalAmount = 100000;
-        bool cashMashine1;
-        bool cashMashine2;
-       
-        
-        int Withdraw(int ammountToWithdraw)
-        {
-            if (totalAmount < 0)
-            {
-                throw new Exception("Crvena zona");
-            }
-            
-                if (totalAmount >= ammountToWithdraw)
-                {
-
-                    Console.WriteLine("Stanje na racunu u banci: {0}", totalAmount);
-                    Console.WriteLine("Iznos koji klijent podize: {0}", ammountToWithdraw);
-                    totalAmount -= ammountToWithdraw;
-                    Console.WriteLine("Stanje na racunu nakon podizanja: {0}\n", totalAmount);
-                    return ammountToWithdraw;
-                }
-                else
-                {
-                    return 0; // Transakcija odbijena
-                }
-            
-
-        }
-       public void DoTransactionsOnCashMashine1(int ammountToWithdraw)
-       {
-            lock (thisLock)
-            {
-                if (cashMashine2)
-                {
-
-                    while (cashMashine2)
-                    {
-                        Thread.Sleep(25);
-                    }
-
-                    Console.WriteLine("Klijent {0} pokusava da podigne {1} RSD sa bankomata1",
-                        Thread.CurrentThread.Name, ammountToWithdraw);
-                    cashMashine1 = true;
-                    int withdrawnAmount = Withdraw(ammountToWithdraw);
-                    if (withdrawnAmount == 0)
-                    {
-                        Console.WriteLine("Nema dovoljno novca na racunu banke");
-                    }
-                    Thread.Sleep(1000);
-                    cashMashine1 = false;
-
-                }
-                else
-                {
-                    Console.WriteLine("Klijent {0} pokusava da podigne {1} RSD sa bankomata1",
-                     Thread.CurrentThread.Name, ammountToWithdraw);
-                    cashMashine1 = true;
-                    int withdrawnAmount = Withdraw(ammountToWithdraw);
-                    if (withdrawnAmount == 0)
-                    {
-                        Console.WriteLine("Nema dovoljno novca na racunu banke");
-                    }
-                    Thread.Sleep(1000);
-                    cashMashine1 = false;
-                }
-            }
-        }
-
-        public void DoTransactionsOnCashMashine2(int ammountToWithdraw)
-        {
-            lock (thisLock)
-            {
-                if (cashMashine1)
-                {
-
-                    while (cashMashine1)
-                    {
-                        Thread.Sleep(25);
-                    }
-                    Console.WriteLine("Klijent {0} pokusava da podigne {1} RSD sa bankomata2",
-                     Thread.CurrentThread.Name, ammountToWithdraw);
-                    cashMashine2 = true;
-                    int witdrawnAmount = Withdraw(ammountToWithdraw);
-                    if (witdrawnAmount == 0)
-                    {
-                        Console.WriteLine("Nema dovoljno novca na racunu banke");
-                    }
-                    Thread.Sleep(1000);
-                    cashMashine2 = false;
-
-                }
-                else
-                {
-
-                    Console.WriteLine("Klijent {0} pokusava da podigne {1} RSD sa bankomata2" +
-                        "",
-                     Thread.CurrentThread.Name, ammountToWithdraw);
-                    cashMashine2 = true;
-                    int witdrawnAmount = Withdraw(ammountToWithdraw);
-                    if (witdrawnAmount == 0)
-                    {
-                        Console.WriteLine("Nema dovoljno novca na racunu banke");
-                    }
-                    cashMashine2 = false;
-                }
-            }
-        }
-    }
+  
         class Program
         {
             static void Main(string[] args)
@@ -127,7 +17,12 @@ namespace DAN_XXXIV_Dejan_Prodanovic
               Queue<Thread> cashMashine1Clients = new Queue<Thread>();
               Queue<Thread> cashMashine2Clients = new Queue<Thread>();
 
-              for (int i = 0; i < 10; i++)
+              Console.WriteLine("Unesite broj klijenata koji podizu novac sa prvog bankomata:");
+              int firstMashineQueueLength = IntInput();
+              Console.WriteLine("Unesite broj klijenata koji podizu novac sa drugog bankomata:");
+              int secondMashineQueueLength = IntInput();
+
+            for (int i = 0; i < 10; i++)
               {
                  Thread t1 = new Thread(() => bank.DoTransactionsOnCashMashine1(rnd.Next(10,10000)));
                 t1.Name = String.Format("Klijent{0}_Bankomat1",i+1);
@@ -144,7 +39,22 @@ namespace DAN_XXXIV_Dejan_Prodanovic
             
             Console.ReadLine();
           }
-        }
+
+          static int IntInput()
+          {
+            bool succes=false;
+            int value;
+            do
+            {
+                succes = Int32.TryParse(Console.ReadLine(),out value);
+                if (!succes || value <=0)
+                {
+                    Console.WriteLine("Nevalidan unos. Unesite pozitivan broj");
+                }
+            } while (!succes || value <= 0);
+            return value;
+          }
+       }
 
     }
  
